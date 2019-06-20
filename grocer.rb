baf08aed3a56458da5cd8d908eadaae3265194c4
Daniel Voigt <danielhvoigt@gmail.com>
2019-06-20 16:54:29 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -4,6 +4,7 @@ def consolidate_cart(cart)
     item_name = item.keys[0]
     consolidated_cart[item_name] = item.values[0]
     if consolidated_cart[item_name][:count]
+      consolidated_cart[item_name][:count]+=1
       
 end
 
